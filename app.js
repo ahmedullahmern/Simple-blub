6b91fac49969fcc218bfd846e2d1544abf87eb6c
Ahmedullah <160387491+ahmedullahmern@users.noreply.github.com>
2024-05-16 12:30:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var offblub = document.getElementById("offblub");
+var onblub = document.getElementById("onblub");
+
+function offchal() {
+    offblub.style.display = "block";
+    onblub.style.display = "none";
+}
+
+function onchal() {
+    onblub.style.display = "block";
+    offblub.style.display = "none";
+}
+
+
+
